aaefc825e00015891c218372077db609a49172b0
vuhoangnam30699 <vuhoangnam30699@gmail.com>
2023-02-24 23:30:44 +0700

    Min_Max.cpp

---
+++ Min_Max.cpp
@@ -0,0 +1,32 @@
+// Nhap n so tu ban phim, Tìm giá tri Max, giá tri Min trong dãy so vua nhap.
+
+
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+	int n, i, a[100], max, min;
+	printf("Nhap so phan tu cua day N = ");
+	scanf("%d", &n);
+	for(i=1; i<=n; i++){
+		printf("a[%d] : ", i);
+		scanf("%d", &a[i]);
+	}
+	max=a[1];
+	for(i=1; i<=n; i++){
+		if(a[i]>max){
+			max=a[i];
+		}
+	}
+	printf("\nGia tri lon nhat la : %d", max);
+	min=a[1];
+	for(i=1; i<=n; i++){
+		if(a[i]<min){
+			min=a[i];
+		}
+	}
+	printf("\nGia tri nho nhat la : %d", min);
+	
+	return 0;
+}
